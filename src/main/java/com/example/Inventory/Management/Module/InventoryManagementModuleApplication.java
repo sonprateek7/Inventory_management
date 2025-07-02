0f502def701fbd5a8000efceb0b6ddf5b46cdef7
sonprateek7 <sonprateek7@gmail.com>
2025-07-02 20:05:16 +0530

    Initial Commit

---
+++ src/main/java/com/example/Inventory/Management/Module/InventoryManagementModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventory.Management.Module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Inventory/Management/Module/InventoryManagementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventory.Management.Module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
